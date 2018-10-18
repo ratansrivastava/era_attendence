5cb584ec5b6f0071c4cc39a8b70494f4d3bc0b2c
ratansrivastava <ratan.srivastava885@gmail.com>
2018-10-18 11:51:10 +0530

    Update finalcode.py

--- finalcode.py
+++ finalcode.py
@@ -1,3 +1,11 @@
 import serial
-ser = serial.Serial('dev/ttyUSB0',9600) # for serial lcd
-
+ser = serial.Serial('dev/ttyUSB0',57600) # for serial lcd
+def font_m():
+  ser.write("#;")
+def font_s():
+  ser.write("&;")
+def lcd_clear():
+  ser.write("!;")
+def lcd_print(str):
+   ser.write(str)
+   ser.write("\n;")
